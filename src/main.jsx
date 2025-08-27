f3511636e57ab54943d6391ebb87b12279be659f
Transformação <robertokizirian@hotmail.com>
2025-08-27 19:09:11 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <h1>🚀 RKMMAX está rodando!</h1>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
