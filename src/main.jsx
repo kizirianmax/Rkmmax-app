0cf4e448eea7e2d76161186dcb8bf86e7622fa0f
Transformação <robertokizirian@hotmail.com>
2025-08-28 19:46:39 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <h1>🚀 RKMMAX está rodando!</h1>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
