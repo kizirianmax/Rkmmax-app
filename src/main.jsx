ef8af69e7b312794881d9b147ec0a1527dc74e30
Transformação <robertokizirian@hotmail.com>
2025-09-01 13:09:18 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "../styles.css"; // se seu styles.css está na raiz; se estiver em src, troque para "./styles.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
