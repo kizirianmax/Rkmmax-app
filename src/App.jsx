368927226f85229f263ddc22e08ad162e08593b0
Transformação <robertokizirian@hotmail.com>
2025-09-04 18:59:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css"; // importa os estilos globais
+
+// Páginas
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import ResetPassword from "./pages/ResetPassword";
 import Agents from "./pages/Agents";
 import AgentDetail from "./pages/AgentDetail";
-import PlansScreen from "./pages/PlansScreen";
-import ResetPassword from "./pages/ResetPassword";
 import Subscribe from "./pages/Subscribe";
-import Auth from "./pages/Auth";
-import ErrorBoundary from "./ErrorBoundary";
+import PlansScreen from "./pages/PlansScreen";
 
-export default function App() {
+function App() {
   return (
-    <ErrorBoundary>
-      <Router>
+    <Router>
+      <div className="app-container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/agents" element={<Agents />} />
           <Route path="/agents/:id" element={<AgentDetail />} />
-          <Route path="/plans" element={<PlansScreen />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/subscribe" element={<Subscribe />} />
-          <Route path="/auth" element={<Auth />} />
+          <Route path="/plans" element={<PlansScreen />} />
         </Routes>
-      </Router>
-    </ErrorBoundary>
+      </div>
+    </Router>
   );
 }
+
+export default App;
