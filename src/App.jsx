c502591cb5a16af4d148a856db5fea8178c87064
Transformação <robertokizirian@hotmail.com>
2025-09-04 22:49:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
+
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css"; // importa os estilos globais
 
-// Páginas
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import ResetPassword from "./pages/ResetPassword";
-import Agents from "./pages/Agents";
-import AgentDetail from "./pages/AgentDetail";
 import Subscribe from "./pages/Subscribe";
 import PlansScreen from "./pages/PlansScreen";
+import Account from "./pages/Account";
+
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
+      <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/agents" element={<Agents />} />
-          <Route path="/agents/:id" element={<AgentDetail />} />
           <Route path="/subscribe" element={<Subscribe />} />
           <Route path="/plans" element={<PlansScreen />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </div>
     </Router>
