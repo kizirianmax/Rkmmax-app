573f8bb780d93fead220f5221d878b34a156b0fe
Transformação <robertokizirian@hotmail.com>
2025-09-17 15:58:58 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,27 @@
 // src/components/Navbar.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // opcional se quiser separar estilos
+import "./../index.css"; // usa o CSS global
 
-export default function Navbar() {
+const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/">RKMMax</Link>
       </div>
+
       <ul className="navbar-links">
-        <li>
-          <Link to="/">Início</Link>
-        </li>
-        <li>
-          <Link to="/agentes">Agentes</Link>
-        </li>
-        <li>
-          <Link to="/info">Info</Link>
-        </li>
-        <li>
-          <Link to="/planos">Planos</Link>
-        </li>
+        <li><Link to="/agents">Agentes</Link></li>
+        <li><Link to="/plans">Planos</Link></li>
+        <li><Link to="/info">Info</Link></li>
       </ul>
+
       <div className="navbar-actions">
-        <Link to="/login" className="btn-login">
-          Entrar
-        </Link>
-        <Link to="/signup" className="btn-signup">
-          Cadastrar
-        </Link>
+        <Link to="/login" className="btn btn-outline">Entrar</Link>
+        <Link to="/subscribe" className="btn btn-primary">Criar Conta</Link>
       </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
