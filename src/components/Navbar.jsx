06414133a40adf06e4effde13b9eead9f1c0cb6d
Transformação <robertokizirian@hotmail.com>
2025-09-17 10:10:56 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
+// src/components/Navbar.jsx
 import React from "react";
-import "../styles.css"; // garante que o CSS global seja aplicado
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // opcional se quiser separar estilos
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="nav-left">
-        <a href="/">RKMMAX</a>
+      <div className="navbar-logo">
+        <Link to="/">RKMMax</Link>
       </div>
-
-      <div className="nav-links">
-        <a href="/agentes">Agentes</a>
-        <a href="/info">Info</a>
-        <a href="/planos">Planos</a>
-      </div>
-
-      <div className="nav-cta">
-        <a href="/login" className="btn ghost">Entrar</a>
-        <a href="/signup" className="btn primary">Criar conta</a>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Início</Link>
+        </li>
+        <li>
+          <Link to="/agentes">Agentes</Link>
+        </li>
+        <li>
+          <Link to="/info">Info</Link>
+        </li>
+        <li>
+          <Link to="/planos">Planos</Link>
+        </li>
+      </ul>
+      <div className="navbar-actions">
+        <Link to="/login" className="btn-login">
+          Entrar
+        </Link>
+        <Link to="/signup" className="btn-signup">
+          Cadastrar
+        </Link>
       </div>
     </nav>
   );
