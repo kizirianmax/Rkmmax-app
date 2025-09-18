9232a1ccf01f1ad3a9ca0bdb3c1b0a3bfb32992e
Transformação <robertokizirian@hotmail.com>
2025-09-18 11:55:14 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 // src/App.jsx
 import React from "react";
 import Navbar from "./components/Navbar";
-import PricingTable from "./components/PricingTable";
-import AgentsList from "./components/AgentsList"; // ✅ Importamos a lista de agentes
+import AgentsList from "./components/AgentsList";
+// import PricingTable from "./components/PricingTable"; // ← TEMP: comente
 
-function App() {
+export default function App() {
   return (
     <div>
-      {/* Barra de navegação */}
       <Navbar />
-
-      {/* Seção de agentes */}
       <main>
         <AgentsList />
       </main>
-
-      {/* Seção de planos */}
-      <section style={{ marginTop: "3rem" }}>
+      {/* <section style={{ marginTop: "3rem" }}>
         <PricingTable />
-      </section>
+      </section> */}
     </div>
   );
 }
-
-export default App;
