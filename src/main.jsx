3fc8edf3364dca7ad33ccdfa01d51db570ce1e75
Transformação <robertokizirian@hotmail.com>
2025-09-18 17:49:01 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css"; // se você usa o CSS global aqui
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
