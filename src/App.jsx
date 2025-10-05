7d2e8783e83018d444ab04ccaf95f08ab5f0db96
Transformação <robertokizirian@hotmail.com>
2025-10-05 13:13:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Header from "./components/Header";
+import BrandTitle from "./components/BrandTitle";
+
 import Home from "./pages/Home";
 import AgentsPage from "./pages/Agents";
 import Pricing from "./pages/Pricing";
@@ -10,14 +12,21 @@ import Pricing from "./pages/Pricing";
 export default function App() {
   return (
     <BrowserRouter>
+      {/* Define o título da aba com base na marca */}
+      <BrandTitle />
+
+      {/* Navegação */}
       <Header />
+
+      {/* Rotas */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agents" element={<AgentsPage />} />
-        {/* Mantém a rota antiga */}
+        {/* Rota antiga mantida */}
         <Route path="/pricing" element={<Pricing />} />
-        {/* Alias para o botão/links que usam /plans */}
+        {/* Alias: permite usar /plans também */}
         <Route path="/plans" element={<Pricing />} />
+        {/* Fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
