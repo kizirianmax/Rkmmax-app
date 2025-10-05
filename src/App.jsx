36ff13485b38eefc7575b05febf535b45f04d5e0
Transformação <robertokizirian@hotmail.com>
2025-10-05 13:07:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import BrandTitle from "./components/BrandTitle";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import AgentsPage from "./pages/Agents";
@@ -11,12 +10,14 @@ import Pricing from "./pages/Pricing";
 export default function App() {
   return (
     <BrowserRouter>
-      <BrandTitle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agents" element={<AgentsPage />} />
+        {/* Mantém a rota antiga */}
         <Route path="/pricing" element={<Pricing />} />
+        {/* Alias para o botão/links que usam /plans */}
+        <Route path="/plans" element={<Pricing />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
