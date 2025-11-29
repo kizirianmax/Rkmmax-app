be6fd1028921a44e83025a89760b92d2abc056ec
kizirianmax <220039442+kizirianmax@users.noreply.github.com>
2025-11-29 03:46:26 -0500

    🔧 fix: Converter OptimizedAPIManager para ES6 export default
    - Remover module.exports (CommonJS)
    - Usar export default (ES6)
    - Corrigir compatibilidade com import em React

--- src/api/OptimizedAPIManager.js
+++ src/api/OptimizedAPIManager.js
@@ -536,12 +536,6 @@ class OptimizedAPIManager {
   }
 }
 
-// EXPORTAR PARA COMMONJS E ES6
-module.exports = OptimizedAPIManager;
-module.exports.default = OptimizedAPIManager;
-
-// PARA COMPATIBILIDADE COM ES6 IMPORTS
-if (typeof window !== 'undefined') {
-  window.OptimizedAPIManager = OptimizedAPIManager;
-}
+// EXPORTAR COMO ES6 DEFAULT
+export default OptimizedAPIManager;
 
