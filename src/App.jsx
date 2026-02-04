cf405ca49be0d5e6a119ee792c3af44fb89ee035
RKMMAX INFINITY MATRIX STUDY <robertokizirian@hotmail.com>
2026-02-04 02:22:15 -0300

    Updated App.jsx to add BetinhoPage import and route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import BetinhoPage from './pages/BetinhoPage';
+'use strict';
 
-// ... other imports
+import React from 'react';
+import { Route } from 'react-router-dom';
+import RequireSubscription from './RequireSubscription';
+import BetinhoPage from './pages/BetinhoPage.jsx';
 
-<Route path="/betinho" element={<BetinhoPage />} />
+// existing routes ...
+
+const App = () => (
+  <div>
+    <RequireSubscription>
+      {/* existing route */}
+      <Route path='/betinho' component={BetinhoPage} />
+      <RequireSubscription forAgents>
+        {/* nested routes for agents... */}
+      </RequireSubscription>
+    </RequireSubscription>
+  </div>
+);
+
+export default App;
