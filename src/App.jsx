332b344192812a27104132639428508600f4aee5
RKMMAX INFINITY MATRIX STUDY <robertokizirian@hotmail.com>
2026-02-06 21:44:39 -0300

    Add RequireSubscription protection to AI routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import BetinhoPage from './pages/BetinhoPage.jsx';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import RequireSubscription from "./components/RequireSubscription.jsx";
+import Serginho from './components/Serginho';
+import Projects from './components/Projects';
+import StudyLab from './components/StudyLab';
+import PlanGate from './components/PlanGate';
+import Specialists from './components/Specialists';
+import SpecialistChat from './components/SpecialistChat';
+import SourceProof from './components/SourceProof';
+import Cronograma from './components/Cronograma';
+import GeradorResumos from './components/GeradorResumos';
+import Flashcards from './components/Flashcards';
+import MapasMentais from './components/MapasMentais';
 
-// ... existing imports
-
-function App() {
-    return (
-        <RequireSubscription>
-            {/* existing routes */}
-            <Route path='/hybrid' component={HybridPage} />
-            <Route path='/betinho' component={BetinhoPage} />
-            {/* other routes */}
-        </RequireSubscription>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/serginho"><RequireSubscription><Serginho /></RequireSubscription></Route>
+        <Route path="/projects"><RequireSubscription><Projects /></RequireSubscription></Route>
+        <Route path="/study"><RequireSubscription><StudyLab /></RequireSubscription></Route>
+        <Route path="/agents"><RequireSubscription><PlanGate /></RequireSubscription></Route>
+        <Route path="/specialists"><RequireSubscription><Specialists /></RequireSubscription></Route>
+        <Route path="/specialist/:specialistId"><RequireSubscription><SpecialistChat /></RequireSubscription></Route>
+        <Route path="/source-proof"><RequireSubscription><SourceProof /></RequireSubscription></Route>
+        <Route path="/cronograma"><RequireSubscription><Cronograma /></RequireSubscription></Route>
+        <Route path="/resumos"><RequireSubscription><GeradorResumos /></RequireSubscription></Route>
+        <Route path="/flashcards"><RequireSubscription><Flashcards /></RequireSubscription></Route>
+        <Route path="/mapas-mentais"><RequireSubscription><MapasMentais /></RequireSubscription></Route>
+        {/* Keep all other routes unchanged */}
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
