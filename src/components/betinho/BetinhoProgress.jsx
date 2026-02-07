971a15196d40566aa5ff97761d9ee67f7b4d70c7
RKMMAX INFINITY MATRIX STUDY <robertokizirian@hotmail.com>
2026-02-06 23:07:42 -0300

    Create BetinhoProgress.jsx file

--- src/components/betinho/BetinhoProgress.jsx
+++ src/components/betinho/BetinhoProgress.jsx
@@ -26,7 +26,9 @@ export default function BetinhoProgress({ progress }) {
             style={{ width: `${percentage}%` }}
           />
         </div>
-        <span style={{ fontSize: '0.875rem', fontWeight: 600 }}>{percentage}%</span>
+        <span style={{ fontSize: '0.875rem', fontWeight: 600 }}>
+          {percentage}%
+        </span>
       </div>
 
       {total > 1 && (
